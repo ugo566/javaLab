93e85df96435f2e1c52a1d1acef2026628ef21c9
안준규 <@DESKTOP-FR1247E>
2021-03-30 17:56:25 +0900

    문제

---
+++ verify/src/verify/exam06.java
@@ -0,0 +1,14 @@
+package verify;
+
+public class exam06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte[] bytes = {73, 32, 108, 111, 118, 101, 32, 121, 111, 117};
+		
+		String str1 = new String(bytes);
+		System.out.println(str1);
+
+	}
+
+}
---
+++ verify/src/생성자/CarExample.java
@@ -0,0 +1,20 @@
+package 생성자;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car c1 = new Car();
+		Car c2 = new Car();
+		c1.model = "제네시스";
+		c1.color = "검정";
+		c1.number = 1004;
+		c1.run(80);
+		
+	
+		
+		
+		
+	}
+
+}
