1de977e1ddf9d4eeb40c0751e4af751e87d93fa0
ugo566 <ugo566@naver.com>
2021-04-07 13:20:42 +0900

    asd244124124

--- chap15Lab/src/verify/exam00/SetTest01.java
+++ chap15Lab/src/verify/exam00/SetTest01.java
@@ -1,29 +1,36 @@
 package verify.exam00;
 
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
-import java.util.Set;
+import java.util.TreeSet;
 
 public class SetTest01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int n;
-		Set s = new HashSet();
+		TreeSet s = new TreeSet();
 		// TreeSet s = new TreeSet();
 		Random r = new Random();
 		// 작성위치
 		// set을 이용하여 로또프로그램만들기
 
-		while (true) {
-			n = r.nextInt(45) + 1;
-			s.add(n);
-			if (s.size() == 6) {
-				System.out.println(s);
-				break;
+		while (s.size() != 6) {
+				n = r.nextInt(45) +1;
+				s.add(n);
 			}
+		System.out.println(s);
 
 		}
-
-	}
+		
+		//정렬 : set -> list로 변경하여 Collection.sort()이용하여 정렬한다.
+//		List list = new ArrayList();
+//		Collections.sort(list);
+//		
+//		System.out.println(s);
+//		System.out.println(list);
+//
+//	}
 }
